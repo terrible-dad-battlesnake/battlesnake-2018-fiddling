704db201745dbb936db4a83692c5dfe28362f9d1
AJ Po-Deziel <aj.podeziel@gmail.com>
2018-02-02 20:49:47 -0800

    Deprecating path_finding for new pathfinding

---
+++ path_finding.py
@@ -0,0 +1,42 @@
+@deprecated
+# from bottle import request, app
+# from game_objects import World, Snake
+#
+# snake_app = app()
+#
+#
+# @snake_app.post('/start')
+# def start():
+#     return {
+#         'color': 'blue',
+#         'name': 'Mr. Windy Miney',
+#         'taunt': 'Weavin\' my \'round town',
+#         'head_type': 'pixel',
+#         'tail_type': 'pixel',
+#         'secondary_color': 'red'
+#     }
+#
+#
+# @snake_app.post('/move')
+# def move():
+#     world = World(request.json)
+#     me = Snake(world.you)
+#
+#     body_buffer = []
+#     buffer_val = 1
+#
+#     for body_piece in me:
+#         current_x = body_piece[0]
+#         current_y = body_piece[1]
+#
+#         if ():
+#
+#         body_buffer.append()
+#
+#     # Create buffer around the snake
+#     if ()
+#     return {
+#         'move': request_move
+#     }
+#
+# snake_app.run(host='0.0.0.0')
