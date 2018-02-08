42904cc73428597ea9738b9099e765bd9fa7c038
Charlie Friend <charles.d.friend@gmail.com>
2018-02-07 17:19:18 -0800

    Guide Dijkstra snake to food.

--- dijkstrasnek.py
+++ dijkstrasnek.py
@@ -1,3 +1,8 @@
+"""
+Pretty dumb Dijkstra snake, but makes use of a bunch of handy-dandy utility
+functions and classes I wrote, so good example usage for those interested.
+:author Charlie
+"""
 import bottle
 import pathfinding
 from bottle import request
@@ -19,7 +24,9 @@ def move():
 
     world = World(request.json)
     d, p = pathfinding.dijkstra(world, world.you)
-    path = pathfinding.find_path_dijkstra(0, 0, p)
+
+    nextfood_x, nextfood_y = world.food[0]
+    path = pathfinding.find_path_dijkstra(nextfood_x, nextfood_y, p)
     next_move = pathfinding.get_next_move(world.you.body[0], path)
 
     return {
